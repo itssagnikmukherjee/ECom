16d3a219d9c1abd3bfb07642f01c952756d0bcdc
itssagnikmukherjee <mukherjee.sagnik17@gmail.com>
2024-12-07 00:39:40 +0530

    update ++

--- app/src/main/java/com/sagnikmukherjee/ecomUser/presentation/ui/screens/ProductScreen.kt
+++ app/src/main/java/com/sagnikmukherjee/ecomUser/presentation/ui/screens/ProductScreen.kt
@@ -1,11 +1,11 @@
 package com.sagnikmukherjee.ecomUser.presentation.ui.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.sagnikmukherjee.ecomUser.presentation.viewModel.UserAppViewModel
 
@@ -20,11 +20,13 @@ fun ProductsScreen(viewModel: UserAppViewModel = hiltViewModel()) {
 
     LazyRow {
         items(state.value.productData.size) {
+            Column {
             Text(text = state.value.productData[it]?.productName.toString())
             Text(text = state.value.productData[it]?.productDescription.toString())
             Text(text = state.value.productData[it]?.productActualPrice.toString())
             Text(text = state.value.productData[it]?.productBeforeDiscountPrice.toString())
             Text(text = state.value.productData[it]?.productImage.toString())
+            }
         }
 
     }
